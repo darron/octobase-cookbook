c6772e0825db24e716c0c21aca214549a10d8002
Darron Froese <darron@froese.org>
2013-12-25 05:03:48 -0700

    First commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+name             'octobase'
+maintainer       'Darron Froese'
+maintainer_email 'darron@froese.org'
+license          'Apache 2.0'
+description      'Installs/configures something'
+version          '0.1.0'
+recipe           'octobase::default', 'Installs/configures something'
